a1b62d841e673ee13c3d8b7165fa3e76b8400b4b
colomr <colomr@pm.me>
2024-10-12 19:20:50 +0000

    Adding hugo package into idx

---
+++ .idx/dev.nix
@@ -0,0 +1,13 @@
+nix
+{ pkgs, ... }: {
+
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.hugo  
+  ];
+
+  # ... rest of your configuration ...
+}
